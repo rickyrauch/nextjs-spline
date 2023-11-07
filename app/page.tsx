9213e7f88c000709743b519c822a360e669f0938
Ricky Rauch <rickyrauch@gmail.com>
2023-11-06 19:16:44 -0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Example() {
   })
   return(
     <div>
-    <Spline style={{'width': 500, 'height': 500}}  scene="https://prod.spline.design/x22ii1Wh66DA-0cv/scene.splinecode" />
+    <Spline style={{'width': 500, 'height': 500}}  scene="https://prod.spline.design/ASwmCWSRU1-3Gfh9/scene.splinecode" />
 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
